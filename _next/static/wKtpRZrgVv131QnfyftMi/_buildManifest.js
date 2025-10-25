507cbf01771b81045d4ce49e2fedf2d0a1bed541
tichuu77 <akashtikhat50@gmail.com>
2025-10-25 22:14:02 +0530

    Updates

--- _next/static/wKtpRZrgVv131QnfyftMi/_buildManifest.js
+++ _next/static/wKtpRZrgVv131QnfyftMi/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/mahalaxmi//_next/:path+",
-        "destination": "/mahalaxmi/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
